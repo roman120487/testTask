31a48b699c810be7b61432b360b27c90dba44ec0
roman120487 <roman120487@ukr.net>
2019-12-06 13:37:32 +0200

    finish

--- script.js
+++ script.js
@@ -51,6 +51,7 @@ calcForm.returnRestBTN.onclick = function () {
 }
 
 // --------------------------------------------------------------
+// -----------------modal window-----------------------
 let container2 = document.querySelector('.container2');
 let modalBtn = document.querySelector('.modalBtn');
 let div = document.createElement('div');
@@ -63,7 +64,6 @@ modalBtn.onclick = function () {
     document.querySelector('.modalBody').append(document.createElement('button'));
     document.querySelector('.modalBody').firstChild.className = 'closeModal';
     document.querySelector('.closeModal').textContent = 'Close';
-    console.log('work');
 
     let closeModal = document.querySelector('.closeModal');
     closeModal.onclick = function () {
@@ -73,4 +73,88 @@ modalBtn.onclick = function () {
 
 
 
-// ------- modal window--------------
+// ------- ------------------------------------------------
+// -------------table-------------------------------
+let formTable = document.forms['tableForm'];
+let tbody = document.querySelector('tbody');
+let td = document.createElement('td');
+let data = new Date();
+let id = 1;
+let regExp = /^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i;
+
+formTable.addBtn.onclick = function () {
+    if (formTable.fName.value === '') {
+        alert('Введіть Ваше імя')
+    } else if (formTable.lName.value === '') {
+        alert('Введіть Ваше прізвище')
+    } else if (formTable.email.value === '') {
+        alert('Введіть e-mail')
+    } else if (regExp.test(formTable.email.value) === true) {
+        tbody.append(document.createElement('tr'));
+        id += 1;
+        tbody.lastChild.id = `t${id}`;
+        tbody.lastChild.append(document.createElement('td'));
+        tbody.lastChild.lastChild.textContent = `${id}`;
+        tbody.lastChild.append(document.createElement('td'));
+        tbody.lastChild.lastChild.textContent = `${data.getDate()}.${data.getMonth() + 1}.${data.getFullYear()} ${data.getHours()}:${data.getMinutes()} `;
+        tbody.lastChild.append(document.createElement('td'));
+        tbody.lastChild.lastChild.textContent = `${formTable.fName.value}`;
+        tbody.lastChild.append(document.createElement('td'));
+        tbody.lastChild.lastChild.textContent = `${formTable.lName.value}`;
+        tbody.lastChild.append(document.createElement('td'));
+        tbody.lastChild.lastChild.textContent = `${formTable.email.value}`;
+        tbody.lastChild.append(document.createElement('td'));
+        tbody.lastChild.lastChild.innerHTML = `<button id="d${id}"  class="delete" onclick="deleteRow(event)">Delete</button>`;
+        tbody.lastChild.append(document.createElement('td'));
+        tbody.lastChild.lastChild.innerHTML = `<button id="e${id}"  class="edit" onclick="editRow(event)">Edit</button>`;
+
+    } else {
+        alert('ви ввели невірний формат e-mail')
+    };
+    resetForm();
+
+}
+let showBtn = true;
+
+function deleteRow(event) {
+    let del = confirm('Ви справді хочете видалити цей запис?')
+    if (del == true) {
+        let idDeleteRow = event.target.id.substr(1);
+        document.querySelector(`#t${idDeleteRow}`).remove();
+    }
+
+}
+
+let idEditRow;
+function editRow(event) {
+    formTable.saveBtn.style.display = 'block';
+    formTable.addBtn.style.display = 'none';
+    idEditRow = event.target.id.substr(1);
+    formTable.fName.value = document.querySelector(`#t${idEditRow}`).childNodes[2].textContent;
+    formTable.lName.value = document.querySelector(`#t${idEditRow}`).childNodes[3].textContent;
+    formTable.email.value = document.querySelector(`#t${idEditRow}`).childNodes[4].textContent;
+}
+
+function saveRow() {
+    if (regExp.test(formTable.email.value) === true) {
+        document.querySelector(`#t${idEditRow}`).childNodes[2].textContent = formTable.fName.value;
+        document.querySelector(`#t${idEditRow}`).childNodes[3].textContent = formTable.lName.value;
+        document.querySelector(`#t${idEditRow}`).childNodes[4].textContent = formTable.email.value;
+    } else {
+        alert('ви ввели невірний формат e-mail');
+    }
+    resetForm();
+    formTable.saveBtn.style.display = 'none';
+    formTable.addBtn.style.display = 'block';
+
+}
+
+function resetForm() {
+    formTable.fName.value = '';
+    formTable.lName.value = '';
+    formTable.email.value = '';
+}
+
+
+
+// ------------------------------------------------
